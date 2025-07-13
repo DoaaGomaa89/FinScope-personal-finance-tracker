f35fbf3013f16167b486b6c1d4d1a86c276870a0
vidhi <vidhiagrawal45@gmail.com>
2025-07-13 17:41:27 +0530

    Update file

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://finscope-1xy6.onrender.com'
+  apiBaseUrl: 'https://finscope-1xy6.onrender.com/api'
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://finscope-1xy6.onrender.com'
+    apiBaseUrl: 'https://finscope-1xy6.onrender.com/api'
 };
